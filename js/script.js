4a6fe31e0e8b18e06b06f5f902cb6bdef4ce80ea
skb-developer <kumarshishant86@gmail.com>
2025-03-20 21:43:58 +0530

    homepage design done

--- js/script.js
+++ js/script.js
@@ -101,3 +101,41 @@ function startCounter(element, target, increment, timeout, suffix) {
         element.innerText = current + suffix;
     }, timeout);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        loop: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 10,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.5,
+                spaceBetween: 20,
+            },
+            900: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+        },
+    });
+});
